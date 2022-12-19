8e114910e93862c9f517925b8335006fa50fa14d
RaeraeSon <100832203+RaeraeSon@users.noreply.github.com>
2022-12-19 19:10:11 +0900

    v1.1

--- js/weather.js
+++ js/weather.js
@@ -1,39 +1,50 @@
 const API_KEY="6217549ff5259121d15f9fda381549f4";
 const COORDS='coords';
-function onGeoOk(position){
-    const lat=position.coords.latitude;
-    const lon=position.coords.longitude;
-    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
-    fetch(url)
-    .then((respone)=>respone.json())
-    .then((json) =>
-        {   
-            const description=json.weather[0].description;
-            const icon=json.weather[0].icon;
-            const temperature=json.main.temp;
-            const currentLocation=json.name;
-            const humidity=json.main.humidity;
-            
-            const weatherLocation=document.querySelector("#weather span:first-child");
-            weatherLocation.innerText=`${currentLocation}`;
-            const weatherTemperature=document.querySelector("#weather span:nth-child(2)");
-            weatherTemperature.innerText=`${temperature.toFixed(1)}℃`;
-            const weatherDescription=document.querySelector("#weather span:last-child");
-            weatherDescription.innerText=`${description}`;
-            console.log(icon);
-    
 
-            /*const weather=document.querySelector("#weather span:first-child");
-            const city=document.querySelector("#weather span:last-child");
-            city.innerText=data.name;
-            weather.innerText=`${data.weather[0].main} / ${data.main.temp.toFixed(1)}`;*/
-            
-            
-        });
+const weatherInfo=document.querySelector("#weatherInfo");
+const weatherIconImg=document.querySelector("#weatherIcon");
+
+function init(){
+    askForCoords();
 }
 
-function onGeoError(){
-    alert("Can't find you. No weather for you.")
+function  askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+function handleSuccess(position){
+    const latitude=position.coords.latitude;
+    const longitude=position.coords.longitude;
+
+    const coordsObj={
+        latitude,
+        longitude
+    };
+
+    getWeather(latitude,longitude);
+}
+
+function handleError(){
+    weatherInfo.innerText("날씨 정보를 불러오는데 실패했습니다.");
+}
+
+function getWeather(lat, lon){
+    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}&lang=kr`;
+    fetch(url).then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        const temperature=json.main.temp;
+        const place=json.name;
+        const weatherDescription=json.weather[0].description;
+        const weatherIcon=json.weather[0].icon;
+        const weatherIconAdrs=`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+
+        weatherInfo.innerText=`${temperature.toFixed(1)} °C / @${place} / ${weatherDescription}`;
+        weatherIconImg.setAttribute('src', weatherIconAdrs);
+    })
+
+    .catch((error)=>console.log("error:",error));
+}
+
+init();
